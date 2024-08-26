922a68332331e6ae9a545b74d711d257d4e9d233
whatapalaver <ang@whatapalaver.co.uk>
2024-08-26 19:49:53 +0100

    First commit trialling activeresource gem

---
+++ app/models/game.rb
@@ -0,0 +1,5 @@
+class Game < ActiveResource::Base
+  # self.site = "https://whatapalaver.github.io/data/bridge/v2/bridge-hands-skewed.json"
+  self.site = "https://whatapalaver.github.io/data/people/"
+
+end
